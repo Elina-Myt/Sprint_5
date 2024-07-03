d283c74252cc949166804a958459fdfcf8c4a385
Elina Mityagina <mityaginaelina@yandex.ru>
2024-07-03 20:47:26 +0300

    тестирование авторизации и роутинга

---
+++ links.py
@@ -0,0 +1,7 @@
+class Links:
+
+    REGISTRATION_PAGE = "https://stellarburgers.nomoreparties.site/register"
+    START_PAGE = "https://stellarburgers.nomoreparties.site"
+    LOGIN_PAGE = "https://stellarburgers.nomoreparties.site/login"
+    PROFILE_PAGE = "https://stellarburgers.nomoreparties.site/account"
+    FEED_PAGE = "https://stellarburgers.nomoreparties.site/feed"
